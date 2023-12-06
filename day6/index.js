49622a2be32da4f16f9434bcda569555895a57f4
Desumond-Osu <ssc051201@gmail.com>
2023-12-06 13:46:10 +0800

    day 6

---
+++ day6/index.js
@@ -0,0 +1,26 @@
+const fs = require('fs');
+const file = fs.readFileSync(`${__dirname}/input.txt`, 'utf8').split('\n');
+const line = file.map(line => line.trim().split(/\s+/).slice(1).map(Number));
+
+//p1
+const pairs = line[0].map((_, i) => line.map(p => p[i]));
+var total = pairs.reduce((acc, [time, dist]) => {
+    var points = 0;
+    for (var i = 1; i <= time; i++) {
+        if ((time - i) * i > dist) {
+            points++;
+        }
+    }
+    return acc * points;
+}, 1)
+console.log(total);
+
+//p2
+const [time, dist] = [parseInt(line[0].join(''), 10), parseInt(line[1].join(''), 10)]
+var total = 0;
+for (var i = 1; i <= time; i++) {
+    if ((time - i) * i > dist) {
+        total++;
+    }
+}
+console.log(total);
