7fa4ec7cdf3fea1b447415473c29b107e23c5b96
Desumond-Osu <ssc051201@gmail.com>
2023-12-14 21:40:28 +0800

    modify day 14

--- day14.js
+++ day14.js
@@ -13,33 +13,31 @@ function pullOToLeft(platform) {
     return platform.map(row => row.split('#').map(part => Array(part.length).fill('.').fill('O', 0, (part.match(/O/g) || []).length).join('')).join('#'))
 }
 
-for (const part of [1, 2]) {
-    if (part == 1) {
-        var platform = rotateCounterC(file);
-        platform = pullOToLeft(platform);
+//p1
+var platform = rotateCounterC(file);
+platform = pullOToLeft(platform);
+platform = rotateC(platform);
+console.log(platform.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (platform[0].length - index), 0));
+
+
+//p2
+var platform = rotateC(rotateC(file)); //rotate 180
+const log = [];
+const cycles = 1000000000;
+for (let cycle = 0; cycle < cycles; cycle++) {
+    for (let rotation = 0; rotation < 4; rotation++) {
         platform = rotateC(platform);
+        platform = pullOToLeft(platform);
     }
 
-    if (part == 2) {
-        var platform = rotateC(rotateC(file)); //rotate 180
-        const log = [];
-        const cycles = 1000000000;
-        for (let cycle = 0; cycle < cycles; cycle++) {
-            for (let rotation = 0; rotation < 4; rotation++) {
-                platform = rotateC(platform);
-                platform = pullOToLeft(platform);
-            }
-
-            //check if same platform exist in log
-            const logIndex = log.indexOf(platform.toString());
-            if (logIndex != -1) {
-                platform = log[logIndex + (((cycles - 1) - logIndex) % (cycle - logIndex))].split(',');
-                break;
-            }
-
-            log.push(platform.toString());
-        }
-        platform = rotateC(rotateC(platform)); //rotate 180
+    //check if same platform exist in log
+    const logIndex = log.indexOf(platform.toString());
+    if (logIndex != -1) {
+        platform = log[logIndex + (((cycles - 1) - logIndex) % (cycle - logIndex))].split(',');
+        break;
     }
-    console.log(platform.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (platform[0].length - index), 0));
+
+    log.push(platform.toString());
 }
+platform = rotateC(rotateC(platform)); //rotate 180
+console.log(platform.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (platform[0].length - index), 0));
