e8f1103f759accb8e330e1aeefed91373ed72dcc
Desumond-Osu <ssc051201@gmail.com>
2023-12-14 21:50:05 +0800

    modify day 14

--- day14.js
+++ day14.js
@@ -9,34 +9,30 @@ function rotateCounterC(arr) {
     return arr[0].split('').map((_, index) => arr.map(row => row[row.length - 1 - index]).join(''));
 }
 
-function pullOToLeft(platform) {
-    return platform.map(row => row.split('#').map(part => Array(part.length).fill('.').fill('O', 0, (part.match(/O/g) || []).length).join('')).join('#'))
+function pullOToLeft(arr) {
+    return arr.map(row => row.split('#').map(part => Array(part.length).fill('.').fill('O', 0, (part.match(/O/g) || []).length).join('')).join('#'))
+}
+
+function getTotalO(arr) {
+    return arr.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (arr[0].length - index), 0);
 }
 
 //p1
-var platform = rotateCounterC(file);
-platform = pullOToLeft(platform);
-platform = rotateC(platform);
-console.log(platform.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (platform[0].length - index), 0));
+console.log(getTotalO(rotateC(pullOToLeft(rotateCounterC(file)))));
 
 //p2
-var platform = rotateC(rotateC(file)); //rotate 180
-const log = [];
-const cycles = 1000000000;
+var platform = rotateC(rotateC(file));
+const log = [], cycles = 1000000000;
 for (let cycle = 0; cycle < cycles; cycle++) {
     for (let rotation = 0; rotation < 4; rotation++) {
-        platform = rotateC(platform);
-        platform = pullOToLeft(platform);
+        platform = pullOToLeft(rotateC(platform));
     }
 
-    //check if same platform exist in log
-    const logIndex = log.indexOf(platform.toString());
+    const logIndex = log.indexOf(platform.join(','));
     if (logIndex != -1) {
         platform = log[logIndex + (((cycles - 1) - logIndex) % (cycle - logIndex))].split(',');
         break;
     }
-
-    log.push(platform.toString());
+    log.push(platform.join(','));
 }
-platform = rotateC(rotateC(platform)); //rotate 180
-console.log(platform.reduce((acc, val, index) => acc + (val.match(/O/g) || []).length * (platform[0].length - index), 0));
+console.log(getTotalO(rotateC(rotateC(platform))));
