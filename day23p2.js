d485ed2af1108d877ebc724f27ce134e954c4312
Desumond-Osu <ssc051201@gmail.com>
2025-08-04 10:20:24 +0800

    idk what this is, ignore

--- day23p2.js
+++ day23p2.js
@@ -6,13 +6,14 @@ const end = [hikeMap.length - 1, hikeMap.at(-1).indexOf('.')];
 
 const path = { 'u': [-1, 0], 'l': [0, -1], 'd': [1, 0], 'r': [0, 1] };
 const dirOpp = { 'u': 'd', 'l': 'r', 'd': 'u', 'r': 'l' };
-const arrowToDir = { '^': 'u', '<': 'l', 'v': 'd', '>': 'r' };
 const stepsArr = [];
 
-const stack = [[start, 'd', []]];
+const memo = new Set();
+
+const stack = [[start, 'd', [], []]];
 
 do {
-    const [[x, y], dir, steps] = stack.pop();
+    const [[x, y], dir, steps, stepMemo] = stack.pop();
 
     if (hikeMap[x]?.[y] === undefined || hikeMap[x][y] == '#') {
         continue;
@@ -23,7 +24,6 @@ do {
     }
 
     if (x == end[0] && y == end[1]) {
-        // console.log(steps.join('|'), [...new Set(steps.map(tile => tile.split(' ')[0]))].length)
         console.log(steps.length)
         stepsArr.push([...new Set(steps.map(tile => tile.split(' ')[0]))].length);
         continue;
@@ -31,9 +31,24 @@ do {
 
     steps.push(`${x},${y}`);
 
+    let i = 3;
+
     Object.keys(path).filter(key => key !== dirOpp[dir]).forEach(newDir => {
-        stack.push([[x + path[newDir][0], y + path[newDir][1]], newDir, [...steps]]);
+        const newX = x + path[newDir][0];
+        const newY = y + path[newDir][1];
+
+        if (hikeMap[newX]?.[newY] === undefined || hikeMap[newX][newY] == '#' || steps.includes(`${newX},${newY}`)) {
+            i--;
+        }
+
+        stack.push([[newX, newY], newDir, [...steps], [...stepMemo]]);
     });
+
+    if (i == 1) {
+        stepMemo.push(`${x},${y}`);
+    } else if (i > 1) {
+        
+    }
 } while (stack.length > 0);
 
 console.log(Math.max(...stepsArr));
